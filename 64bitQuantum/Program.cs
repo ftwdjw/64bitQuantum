d35fe065e5c283ba3f28aa18ecf81e2d47480f31
ftwdjw <ftwdjw@gmail.com>
2018-03-08 06:40:07 -0600

    first commit

---
+++ 64bitQuantum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace bitQuantum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
